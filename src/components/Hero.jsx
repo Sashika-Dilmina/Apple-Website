ad8e318df84c89438e9efffa9aec5cc7d1081cc9
Sashika Dilmina <sashikadilmina01exed@gmail.com>
2025-08-03 23:54:42 +0530

    nw

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -30,7 +30,8 @@ const Hero = () => {
     }, [])
   return (
         <section className="w-full nav-height bg-black relative">
-        <div className="h-5/6 w-full flex-center flex-col">
+        <div className="h-4/5 w-full flex-center flex-col -translate-y-16 md:-translate-y-10">
+
         <p id= "hero" className="hero-title">iPhone 15 Pro</p>
         <div className="md:w-10/12 w-9/12">
         <video className="pointer-events-none"autoPlay muted playsInline={true} key=
@@ -42,7 +43,7 @@ const Hero = () => {
         <div
             id="cta"
             className="flex flex-col items-center opacity-0
-            translate-y-20"
+            translate-y-[200px] -mt-2"
             >
                 <a href = "#highlights" className="btn">Buy</a>
                 <p className="font-normal text-xl">From $199/month or $999</p>
